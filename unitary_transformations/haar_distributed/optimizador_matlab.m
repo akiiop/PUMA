cb323f6879e373dbf3de7b77b01f0c4fe9e58a9e
LucianoPereiraValenzuela <luciano.pereira.valenzuela@gmail.com>
2025-05-13 19:38:01 +0200

    update

--- unitary_transformations/haar_distributed/optimizador_matlab.m
+++ unitary_transformations/haar_distributed/optimizador_matlab.m
@@ -1,21 +1,25 @@
 
-infids_comparison = NaN(8,2);
+infids_comparison = NaN(8,100,2);
 
-parfor d = 3:10
+for d = 3:3
 
     L = 0;
     infids = h5read("data/HaarDistributed_Dim="+string(d)+"_Lay=Dim+"+string(L)+".h5", '/infi');
-    infids_min = min( infids.' );
-    [infid_worst, index_worst] = max( infids_min );
-    
+    [ infids_sort, index_sort ] = sort( infids,  'descend' );
+
     UU = h5read("HaarUnitary.h5",'/unitarydim='+string(d));
     UUs = UU.r + 1i*UU.i;
-        
-    [~,b]=Multiport_Decomposition(UUs(:,:,index_worst),[],[],10,'MGS');
-    infid_matlab = b;
     
-    infids_comparison(d-2,:) = [ infid_worst , infid_matlab ]
+    infids_temp = NaN(100,2);
+    parfor i = 1:4
+        UU = UUs(:,:,i)
+        [~,b]=Multiport_Decomposition(UU,[],[],10,'MGS');
+        infids_temp(i,:) = [ infids(i), b ];
+    end
+    
+    infids_comparison(d-2,:,:) = infids_temp;
+
+    %save( 'Julia_vs_matlab.mat', "infids_comparison" )
     
 end
 
-save( 'Julia_vs_matlab.mat', "infids_comparison" )
